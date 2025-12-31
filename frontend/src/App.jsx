b7af87f0a978d3f180036dea327c6ddd8585a19f
SHAHEEN M <shaheen010507@gmail.com>
2025-12-31 16:23:22 +0530

    Initialize React frontend for BindAI-Explorer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>BindAI-Explorer</h1>
+      <p>Protein–Ligand Binding Affinity Prediction Platform</p>
+    </div>
+  );
+}
+
+export default App;
